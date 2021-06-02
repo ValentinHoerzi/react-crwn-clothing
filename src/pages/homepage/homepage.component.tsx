cff9c6680338d39a6b06982bdb15efe21e6b23f7
ValentinHoerzi <valentin.hoerzi@gmail.com>
2021-06-02 16:07:05 +0200

    Lesson 1 complete

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/directroy.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div>
+      <Directory />
+    </div>
+  </div>
+);
+
+export default HomePage;
